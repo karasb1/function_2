d2e702df2f70dda1d4f16cbd565f2619203a12d6
karasb1 <bohdankaras456@gmail.com>
2024-09-14 22:21:14 +0300

    The ex3 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void distance(int a, int b)
+{
+	if (a <= b)
+	{
+		cout << a << " ";
+	}
+	distance(++a, b);
+}
+
 int main()
 {
-
+	int num1, num2;
+	cin >> num1 >> num2;
+	distance(num1, num2);
 	return 0;
 }
