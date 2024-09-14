e3e18c8e1b42221f5a03d08d6f72ef38e0ae714b
karasb1 <bohdankaras456@gmail.com>
2024-09-14 21:35:42 +0300

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void print(int n)
+{
+	if (n > 0)
+	{
+		cout << "*";
+	}
+	print(n - 1);
+}
+
+
 int main()
 {
-
+	int N;
+	cin >> N;
+	print(N);
 	return 0;
 }
