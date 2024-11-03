ff9cefb3fddfad9661b7f17b253131c6db76fb30
karasb1 <bohdankaras456@gmail.com>
2024-11-03 18:24:03 +0200

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int m(int a, int b)
+{
+	if (b == 0)
+	{
+		return a;
+	}
+	return m(b, a % b);
+}
+
+
+
 int main()
 {
-
+	int a, b;
+	cin >> a >> b;
+	cout << m(a, b) << endl;
 	return 0;
 }
